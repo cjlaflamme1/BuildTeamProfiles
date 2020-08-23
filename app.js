a029a63ca7723c84dd1d60fb456b72c5728efaa6
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-08-22 20:06:59 -0400

    basic questions and log of inquiry

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+const questions = require('./lib/questions');
 const path = require("path");
 const fs = require("fs");
-
+const employeeGenerator = require('./lib/employeeGenerator');
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employeeList = [];
 
+inquirer.prompt(questions).then(function (answers) {
+    console.log(answers);
+});
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
